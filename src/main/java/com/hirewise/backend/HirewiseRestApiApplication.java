a6bc63feb7eeb6912e2b0b1dabb112a0794578d7
suheda-snr <t3sesu00@students.oamk.fi>
2025-05-08 13:11:04 +0300

    first commit

---
+++ src/main/java/com/hirewise/backend/HirewiseRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hirewise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewiseRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirewiseRestApiApplication.class, args);
+	}
+
+}
